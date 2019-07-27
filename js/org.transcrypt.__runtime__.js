dd0340d82776f7520594109a980980e7ee00d415
Kerfuffle <kerfluffle@keemail.me>
2019-07-27 01:49:55 -0600

    Sync

--- js/org.transcrypt.__runtime__.js
+++ js/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-07-27 01:23:27
+// Transcrypt'ed from Python, 2019-07-27 01:49:34
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
